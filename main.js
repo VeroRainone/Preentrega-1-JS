0815ecae52240c5c90d7a13f3ea0e1ff4fb8ba6e
¨VeroRainone¨ <vrainone@hotmail.com>
2023-06-20 13:33:22 -0300

    preentrega1js

---
+++ main.js
@@ -0,0 +1,90 @@
+
+console.log("Nivel 1: Hasta 1 cifra");
+console.log("Nivel 2: Hasta 2 cifras");
+console.log("Nivel 3: Hasta 3 cifras");
+console.log("Nivel 4: Hasta 4 cifras");
+
+function jugarDeNuevo(){
+    let jugarOtraVez = prompt ("Deseas jugar de nuevo? Si-No");
+    if (jugarOtraVez.toLowerCase() === "no"){
+        alert("Gracias por jugar! Nos vemos la próxima!");
+        return;
+    }else if (jugarOtraVez.toLowerCase() === "si"){
+        ingresarNivel();
+    }else{
+        alert("Opción inválida. Por favor, ingresa Si o No.");  
+        jugarDeNuevo(); 
+    }
+}
+
+function ultimoIntento(){
+    let intento = Number (prompt("ULTIMO INTENTO: Ingresa un número"));
+    if(intento === numeroSecreto){
+        alert ("CORRECTO! Adivinaste el número!");
+    }else{
+        alert("Agotaste tus intentos. El número era " + numeroSecreto);
+    }
+    jugarDeNuevo();
+}
+
+function pista2(){
+        numero = Number(prompt("Ingresa un número"));
+        if (numeroSecreto !== numero){
+            alert ("Incorrecto! No es el número. Te doy otra pista ");
+            if (numeroSecreto < numero){
+                alert ("Pista 2: El numero es menor al que ingresaste.");
+            }else {
+                alert ("Pista 2: El numero es mayor al que ingresaste.");
+            }
+        } else{
+            alert ("CORRECTO! Adivinaste el número!");
+            return;
+        }  
+    ultimoIntento();
+}   
+
+function ingresarNumero(){
+        numero = Number(prompt("Ingresa un número"));
+        if (numeroSecreto !== numero){
+            alert ("Incorrecto! No es el número. Te doy una pista");
+            if(numeroSecreto % 2 === 0){
+                alert("Pista 1: El número es par.");
+            }else if (numeroSecreto % 2 !== 0){
+                alert("Pista 1: El número es impar.");
+            }
+        } else{
+            alert ("CORRECTO! Adivinaste el número!");
+            return;
+        }  
+    pista2(); 
+}
+
+let numeroSecreto;
+function ingresarNivel(){
+    let nivel = Number (prompt("Bienvenidos al juego de adivinar el Número! Ingrese el N° del nivel que quiera jugar (1-4)."));
+    if (nivel >= 1 && nivel <= 4) {
+        if (nivel === 1){
+            numeroSecreto = Math.floor (Math.random()*10);
+            console.log(numeroSecreto);
+            ingresarNumero();
+        } else if (nivel === 2){
+            numeroSecreto = Math.floor (Math.random()*100);
+            console.log(numeroSecreto);
+            ingresarNumero();
+        } else if (nivel === 3){
+            numeroSecreto = Math.floor (Math.random()*1000);
+            console.log(numeroSecreto);
+            ingresarNumero();
+        } else {        
+            numeroSecreto = Math.floor (Math.random()*10000);
+            console.log(numeroSecreto);
+            ingresarNumero();
+        }
+    }else {
+        alert("Nivel inválido. Por favor, ingresa un número de nivel válido (1-4).");
+        ingresarNivel();
+    }
+}
+//Ejecución
+ingresarNivel();
+
